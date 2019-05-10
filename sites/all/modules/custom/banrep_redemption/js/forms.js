ba24e7a7fe1f6bb5056a57a86e0dd111e0139413
aacevedoday <you@example.com>
2019-05-10 09:07:35 -0500

    update

--- sites/all/modules/custom/banrep_redemption/js/forms.js
+++ sites/all/modules/custom/banrep_redemption/js/forms.js
@@ -4,34 +4,37 @@
     Drupal.behaviors.banrep_redemption = {
       attach: function(context, settings) {
         var select = $('select[name="field_studies_carried_out[und][0][field_req_formacion_desempeno][und][0][field_formacion_y_desarrollo][und][hierarchical_select][selects][0]"]');
+        var elements = [
+          '#edit-field-studies-carried-out-und-0-field-titulo',
+          '#edit-field-studies-carried-out-und-0-field-tipo-posgrado',
+          '#edit-field-studies-carried-out-und-0-field-nivel-estudio',
+          '#edit-field-studies-carried-out-und-0-field-lugar-posgrado',
+          '#edit-field-studies-carried-out-und-0-field-university'
+        ];
+
         if(select.val() == settings.banrep_redemption.REQ_FORMATION_ID_DESEMPEÑO){
-          hidden();
+          hidden(elements);
         } 
         select.change(function(){
             if($(this).val() == settings.banrep_redemption.REQ_FORMATION_ID_DESEMPEÑO){
-              hidden();
+              hidden(elements);
             }else{
-              show();
+              show(elements);
             }
         })
       },
     };
 
-
-    function hidden(){
-      $('#edit-field-studies-carried-out-und-0-field-titulo').css('display','none');
-      $('#edit-field-studies-carried-out-und-0-field-tipo-posgrado').css('display', 'none');
-      $('#edit-field-studies-carried-out-und-0-field-nivel-estudio').css('display', 'none');
-      $('#edit-field-studies-carried-out-und-0-field-lugar-posgrado').css('display', 'none');
-      $('#edit-field-studies-carried-out-und-0-field-university').css('display', 'none');
+    function hidden(elemets){
+      elemets.forEach(element => {
+        $(element).css('display','none');  
+      });
     }
 
-    function show(){
-      $('#edit-field-studies-carried-out-und-0-field-titulo').css('display','inline-block');
-      $('#edit-field-studies-carried-out-und-0-field-tipo-posgrado').css('display', 'inline-block');
-      $('#edit-field-studies-carried-out-und-0-field-nivel-estudio').css('display', 'inline-block');
-      $('#edit-field-studies-carried-out-und-0-field-lugar-posgrado').css('display', 'inline-block');
-      $('#edit-field-studies-carried-out-und-0-field-university').css('display', 'none');
+    function show(elemets){
+      elemets.forEach(element => {
+        $(element).css('display','inline-block');
+      });
     }
   
   }(jQuery));
