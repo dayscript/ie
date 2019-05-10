a14c063ec6feadda18f9153f48a1a60ef7117506
aacevedoday <you@example.com>
2019-05-10 08:23:42 -0500

    update

--- sites/all/modules/custom/banrep_redemption/js/forms.js
+++ sites/all/modules/custom/banrep_redemption/js/forms.js
@@ -3,10 +3,10 @@
   
     Drupal.behaviors.banrep_redemption = {
       attach: function(context, settings) {
-        
+        console.log(settings);
         $('select[name="field_studies_carried_out[und][0][field_req_formacion_desempeno][und][0][field_formacion_y_desarrollo][und][hierarchical_select][selects][0]"]')
         .change(function(){
-            if($(this).val() == 15343){
+            if($(this).val() == settings.banrep_redemption.REQ_FORMATION_ID_DESEMPEÑO){
                 $('#edit-field-studies-carried-out-und-0-field-titulo').css('display','none');
                 $('#edit-field-studies-carried-out-und-0-field-tipo-posgrado').css('display', 'none');
                 $('#edit-field-studies-carried-out-und-0-field-nivel-estudio').css('display', 'none');
