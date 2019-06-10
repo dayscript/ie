8f7f39ac4589b8798ca6ce0106c0adbe14cbf989
yruizday <yruiz@dayscript.com>
2019-06-10 14:40:23 -0500

    SEPI

--- sites/all/modules/custom/banrep_gestion_ppi/gestion_ppi_validate.js
+++ sites/all/modules/custom/banrep_gestion_ppi/gestion_ppi_validate.js
@@ -6,56 +6,40 @@
   // add drupal behavior
   Drupal.behaviors.plan_carrera_validate = {
     attach: function (context, settings) {
-
-        var form = $("#banrep-gestion-ppi-form");
-        $('#banrep-gestion-ppi-form input[type="checkbox"]').click(function(){
-            $(this).parent().next().find('textarea').addClass('validar');
+      var form = $("#banrep-gestion-ppi-form");
+      $('#banrep-gestion-ppi-form input[type="checkbox"]').click(function(){
+          $(this).parent().next().find('textarea').addClass('validar');
+      });
+      $('#banrep-gestion-ppi-form .form-submit').click(function(event){
+        event.preventDefault();
+        var validate_rules = {};
+        var validate_messages = {};
+        $('.validar').each(function() {
+            var nombre = $(this).attr('name');
+            validate_rules[nombre] = {required:true};
         });
-        $('#banrep-gestion-ppi-form .form-submit').click(function(event){
-          event.preventDefault();
-          var validate_rules = {};
-          var validate_messages = {};
-          $('.validar').each(function() {
-              var nombre = $(this).attr('name');
-              validate_rules[nombre] = {required:true};
-          });
-          var validator = form.validate({
-            rules: validate_rules,
-            messages: validate_messages,
-          });
-          $.validator.messages.required = 'Campo requerido.';
-          $.validator.messages.validarAutocompletar = 'Entidad no existe.';
-          $.validator.messages.validarCupo = 'El valor execede el cupo de credito.';
-          $.validator.messages.validarVolumen = 'Campo requerido.';
-          if(form.valid()){
-            form.submit();
-          }
-        });
-        $('a.pub_dropdown').click(function(){
-          $content = $('div.pub_dropdown_'+this.id);
-          if ($content.hasClass('active')) {
-            $content.css("display", "none");
-            $content.removeClass( "active" );
-          }else {
-            $content.css("display", "block");
-            $content.addClass( "active" );          
-          }
-        });
-        console.log('this');
-    /*    var validate_messages = {
-        };
-
         var validator = form.validate({
-            rules: validate_rules,
-            messages: validate_messages,
+          rules: validate_rules,
+          messages: validate_messages,
         });
-
         $.validator.messages.required = 'Campo requerido.';
         $.validator.messages.validarAutocompletar = 'Entidad no existe.';
         $.validator.messages.validarCupo = 'El valor execede el cupo de credito.';
-        $.validator.messages.validarVolumen = 'Campo requerido.';*/
-
-   }
- }
-
+        $.validator.messages.validarVolumen = 'Campo requerido.';
+        if(form.valid()){
+          form.submit();
+        }
+      });
+      $('a.pub_dropdown').click(function(){
+        $content = $('div.pub_dropdown_'+this.id);
+        if ($content.hasClass('active')) {
+          $content.css("display", "none");
+          $content.removeClass( "active" );
+        }else {
+          $content.css("display", "block");
+          $content.addClass( "active" );          
+        }
+      });
+    }
+  }
 })(jQuery);
