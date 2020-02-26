cc9205e4bd5c21e707287462d773fde3eb10bbc3
yruizday <yruiz@dayscript.com>
2020-02-26 15:49:55 -0500

    función para ordenamiento de tablas

--- sites/all/modules/custom/banrep_gestion_ppi/gestion_ppi_validate.js
+++ sites/all/modules/custom/banrep_gestion_ppi/gestion_ppi_validate.js
@@ -40,6 +40,44 @@
           $content.addClass( "active" );          
         }
       });
+
+      sortTable('table-201');
+      sortTable('table-206');
+      sortTable('table-211');
+      sortTable('table-2951');
+      sortTable('table-21204');
+      
+      /* Basado en https://www.w3schools.com/howto/howto_js_sort_table.asp */
+      function sortTable(tableId = null) {
+        let table, rows, switching, i, x, y, shouldSwitch;
+        table = document.getElementById(tableId);
+        switching = true;
+        while (switching) {
+          switching = false;
+          if(table !== null) {
+            rows = table.rows;
+            for (i = 1; i < (rows.length - 1); i++) {
+              shouldSwitch = false;
+              x = rows[i].getElementsByTagName("TD")[1];
+              y = rows[i + 1].getElementsByTagName("TD")[1];
+              let x_parts = x.innerText.split('/');
+              let y_parts = y.innerText.split('/');
+              let x_date = new Date(x_parts[2], x_parts[1], x_parts[0]);
+              let y_date = new Date(y_parts[2], y_parts[1], y_parts[0]);
+              if(Date.parse(x_date) !== NaN && Date.parse(y_date) != NaN) {
+                if (Date.parse(x_date) < Date.parse(y_date)) {
+                  shouldSwitch = true;
+                  break;
+                }
+              }
+            }
+            if (shouldSwitch) {
+              rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+              switching = true;
+            }
+          }
+        }
+      }
     }
   }
 })(jQuery);
