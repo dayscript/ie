254b75d3d776c645d852521afc5391e893bb0a66
Pablo <pvillate@dayscript.com>
2020-03-02 10:49:55 -0500

    ajuste indentación función ordenamiento de tablas, funciones para mostrar campos ocultos en iexplorer 11

--- sites/all/modules/custom/banrep_gestion_ppi/gestion_ppi_validate.js
+++ sites/all/modules/custom/banrep_gestion_ppi/gestion_ppi_validate.js
@@ -48,35 +48,35 @@
   
       /* Basado en https://www.w3schools.com/howto/howto_js_sort_table.asp */
       function sortTable(tableId = null) {
-	  let table, rows, switching, i, x, y, shouldSwitch;
-	  table = document.getElementById(tableId);
-	  switching = true;
-	  while (switching) {
-	      switching = false;
-	      if(table !== null) {
-		  rows = table.rows;
-		  for (i = 1; i < (rows.length - 1); i++) {
-	              shouldSwitch = false;
-                      x = rows[i].getElementsByTagName("TD")[1];
-	              y = rows[i + 1].getElementsByTagName("TD")[1];
-	              let x_parts = x.innerText.split('/');
-	              let y_parts = y.innerText.split('/');
-	              let x_date = new Date(x_parts[2], x_parts[1], x_parts[0]);
-	              let y_date = new Date(y_parts[2], y_parts[1], y_parts[0]);
-		      if(Date.parse(x_date) !== NaN && Date.parse(y_date) != NaN) {
-	                  if (Date.parse(x_date) < Date.parse(y_date)) {
-	 		      shouldSwitch = true;
-			      break;
-			  }
-		      }
-	    	  }
-	          if (shouldSwitch) {
-	              rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-		      switching = true;
-	          }
-	      }
-           }
+        let table, rows, switching, i, x, y, shouldSwitch;
+        table = document.getElementById(tableId);
+        switching = true;
+        while (switching) {
+          switching = false;
+          if(table !== null) {
+          rows = table.rows;
+          for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("TD")[1];
+            y = rows[i + 1].getElementsByTagName("TD")[1];
+            let x_parts = x.innerText.split('/');
+            let y_parts = y.innerText.split('/');
+            let x_date = new Date(x_parts[2], x_parts[1], x_parts[0]);
+            let y_date = new Date(y_parts[2], y_parts[1], y_parts[0]);
+              if(Date.parse(x_date) !== NaN && Date.parse(y_date) != NaN) {
+                if (Date.parse(x_date) < Date.parse(y_date)) {
+                  shouldSwitch = true;
+                  break;
+                }
+              }
+            }
+            if (shouldSwitch) {
+              rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+              switching = true;
+            }
+          }
         }
-     }
+      }
+    }
   }
 })(jQuery);
