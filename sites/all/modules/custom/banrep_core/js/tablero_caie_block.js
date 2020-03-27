4b047b11fce723f2d8145f20a5a018796436f6c0
Pablo Villate <paul645@hotmail.com>
2020-03-27 13:20:47 -0500

    elimino funciones de ordenamiento para gestión de publicaciones

--- sites/all/modules/custom/banrep_core/js/tablero_caie_block.js
+++ sites/all/modules/custom/banrep_core/js/tablero_caie_block.js
@@ -8,45 +8,7 @@
                 $('.remodal .msg').html(msg);
                 $(form_selector).slideToggle("slow", function() {});
             });
-            $(document).ready(function() {
-                sortTable('draggableviews-table-dashboard-mis-publicaciones-block');
-            });
 
-            $('.view-id-dashboard_mis_publicaciones').ajaxComplete(function(event, xhr, settings) {
-                sortTable('draggableviews-table-dashboard-mis-publicaciones-block');
-            });
-
-            /* Basado en https://www.w3schools.com/howto/howto_js_sort_table.asp */
-            function sortTable(tableId = null) {
-                let table, rows, switching, i, x, y, shouldSwitch;
-                table = document.getElementById(tableId);
-                switching = true;
-                while (switching) {
-                    switching = false;
-                    if (table !== null) {
-                        rows = table.rows;
-                        for (i = 1; i < (rows.length - 1); i++) {
-                            shouldSwitch = false;
-                            x = rows[i].getElementsByTagName("TD")[5];
-                            y = rows[i + 1].getElementsByTagName("TD")[5];
-                            let x_parts = x.innerText.split('/');
-                            let y_parts = y.innerText.split('/');
-                            let x_date = new Date(x_parts[2], x_parts[1], x_parts[0]);
-                            let y_date = new Date(y_parts[2], y_parts[1], y_parts[0]);
-                            if (Date.parse(x_date) !== NaN && Date.parse(y_date) != NaN) {
-                                if (Date.parse(x_date) < Date.parse(y_date)) {
-                                    shouldSwitch = true;
-                                    break;
-                                }
-                            }
-                        }
-                        if (shouldSwitch) {
-                            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-                            switching = true;
-                        }
-                    }
-                }
-            }
         }
     }
 }(jQuery));
